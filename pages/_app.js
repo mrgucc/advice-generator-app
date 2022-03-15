3504983242a7925bbfc4d3e2ff170e89940b3038
Matthew-gucc <mguccione@ansfederal.com>
2022-03-15 14:59:13 -0400

    added frontendmentor content

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+        <link href='https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap' rel='stylesheet' />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,10 @@ import Head from 'next/head';
 import Link from 'next/link';
 import styled from 'styled-components';
 
-const HelloDiv = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background: lightblue;
-`;
-
 export default function Home() {
-  return <HelloDiv>hello world Test</HelloDiv>;
+  return (
+    <>
+      <div></div>
+    </>
+  );
 }
